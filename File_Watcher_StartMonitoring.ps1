13f166577a9280a153ff11f3f23cb755e7bad5a0
Rinimujee B <rinimujeeb@srmtchn.co.in>
2020-11-25 21:47:01 +0530

    adding files

---
+++ File_Watcher_StartMonitoring.ps1
@@ -0,0 +1,19 @@
+﻿### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
+    $watcher = New-Object System.IO.FileSystemWatcher
+    $watcher.Path = "E:\FileWatcher\Source"
+    $watcher.Filter = "*.*"
+    $watcher.IncludeSubdirectories = $true
+    $watcher.EnableRaisingEvents = $true  
+
+### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
+    $action = { $path = $Event.SourceEventArgs.FullPath
+                $changeType = $Event.SourceEventArgs.ChangeType
+                $logline = "$(Get-Date), $changeType, $path"
+                Add-content "E:\FileWatcher\log.txt" -value $logline
+              }    
+### DECIDE WHICH EVENTS SHOULD BE WATCHED 
+    Register-ObjectEvent $watcher "Created" -Action $action
+    Register-ObjectEvent $watcher "Changed" -Action $action
+    Register-ObjectEvent $watcher "Deleted" -Action $action
+    Register-ObjectEvent $watcher "Renamed" -Action $action
+    while ($true) {sleep 5}
---
+++ shtdwn.ps1
@@ -0,0 +1,11 @@
+﻿Function Lock-WorkStation {
+$signature = @'
+[DllImport("user32.dll", SetLastError = true)]
+public static extern bool LockWorkStation();
+'@
+$name = "Win32LockWorkStation"
+$LockWorkStation = Add-Type -memberDefinition $signature -name $name -namespace Win32Functions -passthru
+$LockWorkStation::LockWorkStation() | Out-Null
+}
+
+Lock-WorkStation
